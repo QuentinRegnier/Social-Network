848549764a14d2f3b25d0c5a8cf1b0cddc51c559
QuentinRegnier <quentin.regnier17@gmail.com>
2024-07-02 23:10:56 +0000

    new feature for vm

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
     define('HOST','localhost');
     define('DB_NAME','web');
     define('USER','root');
-    define('PASS','Nerzus2224!');
+    define('PASS','root');
 
     try{
         $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
